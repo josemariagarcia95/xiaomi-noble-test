0e7bee326d733bb2327180183f95bbf9a69ddaf4
Jose Maria <josemariagarcia95@gmail.com>
2019-07-23 13:16:05 +0200

    More tests with noble and xiaomi 2

--- xiaomi.js
+++ xiaomi.js
@@ -1,17 +1,12 @@
 const MiBand = require('miband');
 const crypto = require('browserify-aes');
 const noble = require('noble');
+const UUID_BASE = (x) => `0000${x}-0000-3512-2118-0009af100700`
+var events = require("events");
+var emitter = new events.EventEmitter();
 
 //const key = new Buffer('30313233343536373839404142434445', 'hex');
 
-noble.on('stateChange', function (state) {
-	if (state === 'poweredOn') {
-		//Filter by Miband id
-		noble.startScanning();
-	} else {
-		noble.stopScanning();
-	}
-});
 
 
 /*
@@ -40,6 +35,83 @@ noble.on('discover', function (peripheral) {
 });
 */
 
-function connectXiaomi() {
-	//todo
+async function connectXiaomi() {
+	return new Promise((resolve, reject) => {
+		noble.on('stateChange', function (state) {
+			if (state === 'poweredOn') {
+				//Filter by Miband id
+				noble.startScanning(['fee0']);
+			} else {
+				noble.stopScanning();
+			}
+		});
+
+		noble.on('discover', function (peripheral) {
+			console.log('Found device with local name: ' + peripheral.advertisement.localName);
+			console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+			noble.stopScanning();
+			peripheral.connect(function (error) {
+				if (!error) {
+					peripheral.discoverServices(['fee1'], function (error, services) {
+						console.log('discovered the following services:');
+						console.log('Name: ' + services[0]);
+						const authService = services[0];
+						authService.discoverCharacteristics([UUID_BASE('0009')], function (error, characteristics) {
+							const authChar = characteristics[0];
+							authChar.on('data', function (data, isNotification) {
+								console.log('Data from auth', data.readUInt8(0));
+							});
+							// to enable notify
+							authChar.subscribe(function (error) {
+								console.log('Auth notification notification on');
+							});
+
+							authChar.writeValue(Buffer.from([0x02, 0x08]));
+							setTimeout(() => reject('Timeout'), 10000);
+							resolve(peripheral);
+						});
+					});
+				}
+			});
+		});
+	});
 }
+
+
+//const xiaomi = connectXiaomi();
+
+function discoverXiaomiServices() {
+	return new Promise((resolve, reject) => {
+		noble.on('stateChange', function (state) {
+			if (state === 'poweredOn') {
+				//Filter by Miband id
+				noble.startScanning(['fee0']);
+			} else {
+				noble.stopScanning();
+			}
+		});
+
+		noble.on('discover', function (peripheral) {
+			console.log('Found device with local name: ' + peripheral.advertisement.localName);
+			console.log('advertising the following service uuid\'s: ' + peripheral.advertisement.serviceUuids);
+			noble.stopScanning();
+			peripheral.connect(function (error) {
+				if (!error) {
+					peripheral.discoverServices(null, function (error, services) {
+						console.log('discovered the following services:');
+						console.log('Name: ' + services[0]);
+						services.forEach(serv => {
+							serv.discoverCharacteristics(null, function (error, characteristics) {
+								console.log('discovered the following characteristics:');
+								for (var i in characteristics) {
+									console.log('Name: ' + characteristics[i].name + ' - uuid: ' + characteristics[i].uuid + ' - Service UUID: ' + characteristics[i]._serviceUuid);
+								}
+							});
+						});
+					});
+				}
+			});
+		});
+	});
+}
+//discoverXiaomiServices();
